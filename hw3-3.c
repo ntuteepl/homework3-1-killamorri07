6b48cc4a9d81d162210576f9fdd3da3fa1dc7a0d
Lusia <34813844+killamorri07@users.noreply.github.com>
2023-10-20 02:25:07 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    int input1, input2;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &input1, &input2);
+
+    int sum1 = 0, sum2 = 0;
+
+    // Sum the digits of input1
+    for (int num = input1; num != 0; num /= 10) {
+        sum1 += num % 10;
+    }
+
+    // Sum the digits of input2
+    for (int num = input2; num != 0; num /= 10) {
+        sum2 += num % 10;
+        
+    }
+
+    
+    return 0;
+}
