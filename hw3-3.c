9838ecc3de67f97443b0e873b429956194858b63
Lusia <34813844+killamorri07@users.noreply.github.com>
2023-10-21 13:01:31 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 
+int sumOfDigits(int num) {
+    int sum = 0;
+    while (num != 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
 int main() {
     int input1, input2;
 
-    printf("Enter two integers: ");
+    //printf("Enter two integers: ");
     scanf("%d %d", &input1, &input2);
 
-    int sum1 = 0, sum2 = 0;
+    int sum1 = sumOfDigits(input1);
+    int sum2 = sumOfDigits(input2);
 
-    // Sum the digits of input1
-    for (int num = input1; num != 0; num /= 10) {
-        sum1 += num % 10;
+    if (sum1 == sum2) {
+        printf("%d\n", sum1);
+        if (input1 < input2) {
+            printf("%d\n", input1);
+        } else if (input2 < input1) {
+            printf("%d\n", input2);
+        } 
+    } else {
+        if (sum1 < sum2) {
+            printf("%d\n", input1);
+        } else {
+            printf("%d\n", input2);
+        }
     }
 
-    // Sum the digits of input2
-    for (int num = input2; num != 0; num /= 10) {
-        sum2 += num % 10;
-        
-    }
-
-    
     return 0;
 }
